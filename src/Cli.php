0131813ccdb4353d318ec761b219a962e2af2f01
Konstantin Korolenko <softslot-com@yandex.ru>
2022-06-08 21:05:59 +0300

    add addTodo; deleteTodo functions

--- src/Cli.php
+++ src/Cli.php
@@ -6,3 +6,17 @@ function start(): void
 {
     echo 'Hello';
 }
+
+function addTodo($todo, $todoList): array
+{
+    $todoList[] = $todo;
+
+    return $todoList;
+}
+
+function deleteTodo($index, $todoList): array
+{
+    unset($todoList[$index]);
+
+    return array_values($todoList);
+}
