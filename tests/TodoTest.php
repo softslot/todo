83f2c449afa2b928845ebdf4fa73944ce3dda59c
Konstantin Korolenko <softslot-com@yandex.ru>
2022-06-08 21:05:44 +0300

    add tests

--- tests/TodoTest.php
+++ tests/TodoTest.php
@@ -4,13 +4,54 @@ namespace Todo\Tests;
 
 use PHPUnit\Framework\TestCase;
 
+use function Todo\Cli\addTodo;
+use function Todo\Cli\deleteTodo;
+
 class TodoTest extends TestCase
 {
-    public function testOne()
+    public function testAddTodo(): void
     {
-        $expected = true;
-        $result = true;
+        $todoList = [];
 
-        $this->assertEquals($expected, $result);
+        $todo = 'Помыть полы';
+
+        $updatedTodoList = addTodo($todo, $todoList);
+
+        $this->assertEquals([$todo], $updatedTodoList);
+    }
+
+    public function testDeleteTodo(): void
+    {
+        $todoList = [
+            'Помыть полы',
+            'Помыть посуду',
+            'Помыть машину'
+        ];
+
+        $index = 1;
+
+        $updatedTodoList = deleteTodo($index, $todoList);
+
+        $expectedTodoList = [
+            'Помыть полы',
+            'Помыть машину'
+        ];
+
+        $this->assertEquals($expectedTodoList, $updatedTodoList);
+    }
+
+    public function testDeleteTodoByErrorIndex(): void
+    {
+        $todoList = [
+            'Помыть полы',
+            'Помыть посуду',
+            'Помыть машину'
+        ];
+
+        $index = 100;
+
+        $updatedTodoList = deleteTodo($index, $todoList);
+
+        $this->assertEquals($todoList, $updatedTodoList);
     }
 }
